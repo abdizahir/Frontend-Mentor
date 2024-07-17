e0f20007e19dc53bfec440c3d9b443726be10d5a
Abdi12312 <faarxgalool226@gmail.com>
2024-07-17 18:28:56 +0300

    article-preview-component-master

--- article-preview-component-master/JS/main.js
+++ article-preview-component-master/JS/main.js
@@ -1,38 +1,18 @@
-
-const prodDetails = document.querySelector(".prod-details");
-const main = document.querySelector("main");
-var mainPostion = main.getBoundingClientRect();
-var mainWidth = mainPostion.width;
-const btn = document.querySelector(".btn"); 
+const btn = document.querySelector(".btn");
+const share = document.querySelector(".share");
 const personInfo = document.querySelector(".person-info");
-const shareIcon = document.querySelector(".fa-share");
+const share1 = document.querySelector(".share-1");
+const main = document.querySelector(".main");
+const mainPostions = main.getBoundingClientRect();
 btn.addEventListener("click", () => {
-    if(mainWidth < 600) {
+    console.log(mainPostions.width);
+    // personInfo.classList.add("hidden");
+    if(mainPostions.width < 600) {
         personInfo.style.display = "none";
     }else {
-        personInfo.style.display = "display";
-        shareIcon.style.display = "none";
+        personInfo.style.display = "flex";
     }
-    const share = document.createElement("div");
-    share.className = "share";
-    prodDetails.appendChild(share);
-    const icons = document.createElement("div");
-    icons.className = "icons";
-    share.appendChild(icons)
-    const shareTxt = document.createElement("span");
-    shareTxt.className = "span-txt";
-    shareTxt.textContent = "S H A R E";
-    icons.appendChild(shareTxt);
-    const facebookIcon = document.createElement("i");
-    facebookIcon.className = "fa-brands fa-square-facebook";
-    icons.appendChild(facebookIcon);
-    const twiter = document.createElement("i");
-    twiter.className = "fa-brands fa-twitter";
-    icons.appendChild(twiter);
-    const pinterest = document.createElement("i");
-    pinterest.className = "fa-brands fa-pinterest";
-    icons.appendChild(pinterest);
-    const shareLogo = document.createElement("i");
-    shareLogo.className = "fa-solid fa-share";
-    share.appendChild(shareLogo);
+    share1.style.display = "none";
+    // share.classList.add("flex");
+    share.style.display = "flex";
 })
